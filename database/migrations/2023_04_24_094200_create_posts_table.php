b419af5977e2bceefa9cd97f676576786b3f0b66
Halva <ing.halvarado.7s@gmail.com>
2023-04-24 05:58:15 -0400

    add: edit migration, add columns

--- database/migrations/2023_04_24_094200_create_posts_table.php
+++ database/migrations/2023_04_24_094200_create_posts_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('content');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+
         });
     }
 
